c2187d32ffceb2d2f6936be5aa3d6c606f09383d
michiel1106 <michielimmink@gmail.com>
2025-09-19 16:12:29 +0200

    more changes

--- src/main/java/com/bikerboys/schematicannon/AllPartialModels.java
+++ src/main/java/com/bikerboys/schematicannon/AllPartialModels.java
@@ -6,15 +6,7 @@ public class AllPartialModels {
 
 	public static final PartialModel
 
-	SCHEMATICANNON_CONNECTOR = block("schematicannon/connector"), SCHEMATICANNON_PIPE = block("schematicannon/pipe"),
-
-	CRAFTING_BLUEPRINT_1x1 = entity("crafting_blueprint_small"),
-	CRAFTING_BLUEPRINT_2x2 = entity("crafting_blueprint_medium"),
-	CRAFTING_BLUEPRINT_3x3 = entity("crafting_blueprint_large");
-
-
-
-
+	SCHEMATICANNON_CONNECTOR = block("schematicannon/connector"), SCHEMATICANNON_PIPE = block("schematicannon/pipe");
 
 
 
